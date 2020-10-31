ca9d4d02e16eaa36a1873fa9a28d63380c69bfd1
Paolo Mercogliano <paolo.mercoglia@gmail.com>
2020-10-31 11:52:52 +0000

    Rotating cannon

---
+++ Assets/Scripts/PlayerCannon.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCannon : MonoBehaviour
+{
+    public Camera cam;
+    public Transform idleTarget;
+    public float rotationSpeed;
+    public GameObject bullet;
+    public GameObject cannon;
+    public Transform bulletSpawnPosition;
+    public float timeToComeBack;
+
+    private Quaternion baseLookRotation;
+    private Quaternion cannonLookRotation;
+    private Vector3 direction;
+    private Vector3 baseDirection;
+    private Vector3 cannonDirection;
+
+    private bool idlePosition = true;
+    private bool shootingPosition = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (idlePosition)
+        {
+            direction = idleTarget.position;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Vector3 objectHit = hit.point;
+                direction = objectHit;
+                idlePosition = false;
+
+                baseDirection = new Vector3(direction.x, transform.position.y, direction.z);
+                cannonDirection = direction;
+
+
+                cannon.transform.LookAt(cannonDirection);
+            }
+        }
+
+ //       direction = direction.normalized;
+        cannonDirection = direction;
+        cannon.transform.LookAt(cannonDirection);
+
+        transform.LookAt(new Vector3(direction.x, transform.position.y, direction.z));
+    }
+
+
+    
+}
