ccb1e03f3f889787094a82ba13c2055213e52e4c
Paolo Mercogliano <paolo.mercoglia@gmail.com>
2020-11-01 09:13:39 +0000

    Enemy controller

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -47,7 +47,7 @@ public class EnemyController : MonoBehaviour
         }
 
         if (health <= 0)
-        {
+        {   
             // Things to do when enemy dies
             Player.GetComponent<PointsAndUpgrades>().killedEnemy("easy");
             // Play some animation
